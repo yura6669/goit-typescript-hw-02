92cb4d4bda923a736c2189d8c58b9352ca9233a7
yura6669 <yurii.shyian.01@gmail.com>
2025-05-22 21:40:56 +0300

    Remove console

--- src/components/ImageCard/ImageCard.tsx
+++ src/components/ImageCard/ImageCard.tsx
@@ -2,7 +2,6 @@ import css from './ImageCard.module.css'
 import { ImageCardProps } from '../../App.types'
 
 const ImageCard = ({ photo, onOpenModal }: ImageCardProps) => {
-  console.log(photo);
     return (
         <div className={css.img} onClick={() => onOpenModal(photo)}>
 		  <img src={photo.urls.small} alt={photo.slug} width="400" height="300" />
